8d7a5232988ae9797b543ff4a47bfefae1a39e88
Adi <68931532+HimalayanPanda@users.noreply.github.com>
2021-03-23 10:46:41 +1100

    setting up

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const router = express.Router();
+
+router.get('/login', (req, res, next) => {
+
+});
+
+router.post('/register', (req, res, next) => {
+
+});
+
+router.delete('/dashboard/:id', (req, res, next) => {
+
+})
+
+module.exports = router;

439aaa3c7d011c8abdc82eaf2fa067c0ed14cf93
Adi <68931532+HimalayanPanda@users.noreply.github.com>
2021-03-30 09:50:15 +1100

    connecting to database using the config file

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 const api = require('./routes/api');
 
-require("dotenv").config();
+//require("dotenv").config();
 
 const app = express();
+
+// Connect to Database
+connectDB();
+
+// Init Middleware
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV != "prod") {
   require("dotenv").config();
 }
-
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 
 app.get("/", (req, res) => {
   res.send("Hello world! Express is here with the help with nodemon.");

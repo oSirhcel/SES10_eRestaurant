2c50f4bd47cb254ec81cf659371118f02248d8f0
tambam <tyylow@gmail.com>
2021-03-30 13:47:21 +1100

    installed material-ui core and icons

---
+++ frontend/client/src/StaffDashboard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '@material-ui/core';
+
+export default class StaffDataGrid extends React.Component {
+  render() {
+    return <Button> This is a button</Button>
+  }
+}

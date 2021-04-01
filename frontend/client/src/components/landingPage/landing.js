0f3d7617477d4dceac07219f3e097b9bece5ab97
oSirhcel <christopherl3@outlook.com>
2021-04-01 19:29:57 +1100

    header for landing page

---
+++ frontend/client/src/components/landingPage/landing.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import Header from './components/landingPage/header';
+// import MainFeaturedPost from './MainFeaturedPost';
+// import FeaturedPost from './FeaturedPost';
+// import Main from './Main';
+// import Sidebar from './Sidebar';
+// import Footer from './Footer';
+// import post1 from './blog-post.1.md';
+// import post2 from './blog-post.2.md';
+// import post3 from './blog-post.3.md';
+
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+const sections = [
+  { title: 'Locations', url: '#' },
+  { title: 'Menu', url: '#' },
+  { title: 'Reserve', url: '#' },
+  { title: 'About Us', url: '#' },
+  { title: 'Promotions', url: '#' },
+  { title: 'My Account', url: '#' },
+];
+
+export default function App() {
+  return (
+  <React.Fragment>
+    <CssBaseline />
+    <Container maxWidth="lg">
+      <Header title="Le Bistrot d'Andre" sections={sections} />
+    </Container>
+  </React.Fragment>
+);
+}
+

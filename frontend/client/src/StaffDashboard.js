95b2943ee986dd5cf43160ac2b8b479732e34135
tambam <tyylow@gmail.com>
2021-04-04 09:31:30 +1000

    deleted unecessary files

--- frontend/client/src/StaffDashboard.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Button from '@material-ui/core';
-
-export default class StaffDataGrid extends React.Component {
-  render() {
-    return <Button> This is a button</Button>
-  }
-}

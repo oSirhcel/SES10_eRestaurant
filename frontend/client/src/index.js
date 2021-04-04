2da2d07a09849811c4ad73a13581be9b7afe0159
tambam <tyylow@gmail.com>
2021-04-05 09:25:18 +1000

    updated material ui packages

--- frontend/client/src/index.js
+++ frontend/client/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import BasicDatePicker from './Date Picker';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BasicDatePicker />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

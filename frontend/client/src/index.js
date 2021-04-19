1961b38dfbdeeb74d8f44b0f0bcaa9e617c31936
tambam <tyylow@gmail.com>
2021-04-19 11:00:39 +1000

    changed index.js to render App function

--- frontend/client/src/index.js
+++ frontend/client/src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 import Stage from './StaffRecordsDataGridController';
 
-ReactDOM.render(<Stage />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 

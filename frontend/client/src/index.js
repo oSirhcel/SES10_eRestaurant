672cb0aa5cc886b05bcff96c1407538802e7a5fc
tambam <tyylow@gmail.com>
2021-04-20 12:44:26 +1000

    fixed imports

--- frontend/client/src/index.js
+++ frontend/client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import Stage from './StaffRecordsDataGridController';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

da996bbd83f7d9038b48104c68aaa9039401d740
tambam <tyylow@gmail.com>
2021-04-23 13:54:43 +1000

    created branch for feature-editMenu

--- frontend/client/src/index.js
+++ frontend/client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
+import MenuDataGrid from './components/edit-Menu/MenuDataGrid';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MenuDataGrid />, document.getElementById('root'));
 

c7aaf27ee1ed53a443d7b5095a1ff514f990a709
tambam <tyylow@gmail.com>
2021-04-28 20:29:32 +1000

    changed index.js file to render App

--- frontend/client/src/index.js
+++ frontend/client/src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 import StaffRecordsDataGridController from './components/staff-records/StaffRecordsDataGridController';
 
-ReactDOM.render(<StaffRecordsDataGridController />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 

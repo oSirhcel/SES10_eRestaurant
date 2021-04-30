fa084fbf0135e5a164b4d8d0871c72646358dc37
oSirhcel <christopherl3@outlook.com>
2021-04-30 14:37:55 +1000

    fixed margins

--- frontend/client/src/components/landingPage/landing.js
+++ frontend/client/src/components/landingPage/landing.js
@@ -1,57 +1,51 @@
-import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
-import Header from './components/landingPage/header';
-import MainFeaturedPost from "./components/landingPage/feature.js"
-import Footer from './components/landingPage/footer';
-import MainFeaturedPost from "./components/landingPage/feature.js"
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import Header from "../margins/header";
+import Footer from "../margins/footer";
+import MainFeaturedPost from "./components/landingPage/feature.js";
 
-const useStyles = makeStyles((theme) => ({ 
+const useStyles = makeStyles((theme) => ({
   root: {
-      marginTop: theme.spacing(0),
-      minHeight: '100vh',
-      
-
-
-  }
+    marginTop: theme.spacing(0),
+    minHeight: "100vh",
+  },
 }));
 
-
-
 const mainFeaturedPost = {
-title: 'The Perfect Crossaint.',
-description:'Experience the signature food of France at its finest.',
-imgText: 'crossaint',
-buttonText: 'Book with us',
+  title: "The Perfect Crossaint.",
+  description: "Experience the signature food of France at its finest.",
+  imgText: "crossaint",
+  buttonText: "Book with us",
 };
 
-
 const sections = [
-{ title: 'LOCATIONS', url: '#' },
-{ title: 'MENU', url: '#' },
-{ title: 'RESERVE', url: '#' },
-{ title: 'ABOUT US', url: '#' },
-{ title: 'PROMOTIONS', url: '#' },
-{ title: 'MY ACCOUNT', url: '#' },
+  { title: "LOCATIONS", url: "#" },
+  { title: "MENU", url: "#" },
+  { title: "RESERVE", url: "#" },
+  { title: "ABOUT US", url: "#" },
+  { title: "PROMOTIONS", url: "#" },
+  { title: "MY ACCOUNT", url: "#" },
 ];
 
-export default function App(){
+export default function App() {
   const classes = useStyles();
-  return <div className={classes.root}>
+  return (
+    <div className={classes.root}>
       <React.Fragment>
-  <CssBaseline />
-  <Container maxWidth="lg" >
-    <Header title="Le Bistrot d'Andre" sections={sections} />
-  </Container>
-  <main>
-    <MainFeaturedPost post={mainFeaturedPost} />
-    <MainDescription />
-    </main>
-    <Container>
-      
-      <Footer/>
-    </Container>
-</React.Fragment>
-  </div>;
+        <CssBaseline />
+        <Container maxWidth="lg">
+          <Header title="Le Bistrot d'Andre" sections={sections} />
+        </Container>
+        <main>
+          <MainFeaturedPost post={mainFeaturedPost} />
+          <MainDescription />
+        </main>
+        <Container>
+          <Footer />
+        </Container>
+      </React.Fragment>
+    </div>
+  );
 }

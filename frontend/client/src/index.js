347ef8ca7e161c72d496cfe5c6f31a70a6708a3a
tambam <tyylow@gmail.com>
2021-05-15 14:53:23 +1000

    updated frontend index

--- frontend/client/src/index.js
+++ frontend/client/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import StaffRecordsStage from './components/staff-records/StaffRecordsDataGridController';
-import CustomerReservationsController from './components/customerReservations/CustomerReservationsController';
 
-ReactDOM.render(<CustomerReservationsController />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 

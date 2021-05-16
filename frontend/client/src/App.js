0a351479cba454d76ceaa52ce107c692dad1f418
Isobel Bennett <isobelsbennett@gmail.com>
2021-05-16 19:21:05 +1000

    removed viewMenu import

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -2,17 +2,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import Routes from "./components/routing/Routes";
-import ViewMenu from "./components/viewMenu/ViewMenu";
 
 export default function App() {
   return (
-    // <Provider store={store}>
-    //   <Router>
-    //     <Route component={Routes} />
-    //   </Router>
-    // </Provider>
-    <div>
-      <ViewMenu />
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Route component={Routes} />
+      </Router>
+    </Provider>
   );
 }

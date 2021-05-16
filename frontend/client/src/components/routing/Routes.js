679d2da5ff82c5c5e67c186ca9eb1246b2344bbf
tambam <tyylow@gmail.com>
2021-05-16 14:25:03 +1000

    saved route changes from makeReservation

--- frontend/client/src/components/routing/Routes.js
+++ frontend/client/src/components/routing/Routes.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-<<<<<<< HEAD
 import RegisterCustomer from "../Register/customer";
 import RegisterStaff from "../Register/staff";
 import ReservationController from '../makeReservation/ReservationController';
-=======
 import MenuTabController from '../edit-Menu/MenuTabController';
->>>>>>> development
 import { BrowserRouter as Router } from "react-router-dom";
 import Alert from "../layout/Alert";
 import Login from "../login/login";
@@ -25,13 +22,10 @@ const Routes = (props) => {
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={RegisterCustomer} />
         <Route exact path="/registerStaff" component={RegisterStaff} />
-<<<<<<< HEAD
         <Route exact path="/makeReservation" component={ReservationController}/>
-=======
         <Route exact path="/staffMenuView" component={MenuTabController} />
         <Route exact path="/customerReservations" component={CustomerReservationsController} />
         <Route exact path="/staffRecords" component={StaffRecordsDataGridController} />
->>>>>>> development
       </Router>
     </section>
   );

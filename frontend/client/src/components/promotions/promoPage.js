d29527cc3e43598493a559603d5b70640d20e3a2
Sirhcle <christopherl3@outlook.com>
2021-05-16 19:47:50 +1000

    added crepe

--- frontend/client/src/components/promotions/promoPage.js
+++ frontend/client/src/components/promotions/promoPage.js
@@ -21,8 +21,8 @@ const useStyles = makeStyles((theme) => ({
     alignContent: "center",
   },
   card: {
-    marginTop: 60,
-    marginBottom: 60,
+    marginTop: 100,
+    marginBottom: 20,
     maxWidth: 750,
     alignContent: "center",
   },
@@ -63,6 +63,7 @@ export default function PromoPage() {
                     variant="body2"
                     color="textSecondary"
                     component="p"
+                    align="center"
                   >
                     Buy 1 croissant get 1 free.
                   </Typography>
@@ -75,17 +76,40 @@ export default function PromoPage() {
               <CardActionArea>
                 <CardMedia
                 className={classes.media}
-                  title="Croque Monsieur"
+                  title="Macaron"
                   component="img"
-                  src='https://secureservercdn.net/198.71.189.253/b6d.5c9.myftpupload.com/wp-content/uploads/2019/10/inside-vegan-croissant-stacked.jpg'
+                  src='https://www.kids-world-travel-guide.com/images/xfrench_food_macarons_shutterstock_62967172-2.jpg.pagespeed.ic.1_Cll_AGWX.jpg'
                 />
                 <CardContent>
                   <Typography
                     variant="body2"
                     color="textSecondary"
                     component="p"
+                    align="center"
                   >
-                    Buy 1 croissant get 1 free.
+                    15% off meals over $20
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={6}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                className={classes.media}
+                  title="Crepe"
+                  component="img"
+                  src='https://www.goway.com/media/cache/70/22/702291eb1a1895e3bc3d124cc5ae4da9.jpg'
+                />
+                <CardContent>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                    align="center"
+                  >
+                    Free crepe with any meal over $35.
                   </Typography>
                 </CardContent>
               </CardActionArea>
